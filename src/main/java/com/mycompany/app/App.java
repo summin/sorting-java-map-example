780a68f1ffe4215eaf9f3463fd7262b198f094a3
Aleksandr Leontjev <a.leontjev@pyrexx.com>
2021-08-11 09:02:24 +0200

    dev

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -30,5 +30,36 @@ public class App
         // expected output:
         // beaver_4, wombat_3, jungle_2, koala_2
 
+        Map<String, Integer> sorted = new TreeMap<>();
+
+        for (String transaction : transactions) {
+            if (!sorted.containsKey(transaction)) {
+                sorted.put(transaction, 1);
+                continue;
+            }
+            sorted.put(transaction, sorted.get(transaction) + 1);
+        }
+
+        class Transaction {
+            public final String transaction;
+            public final Integer amount;
+            Transaction(String transaction, Integer amount) {
+                this.transaction = transaction;
+                this.amount = amount;
+            }
+        }
+
+        List<Transaction> transactionsList = new ArrayList<Transaction>();
+
+        for (Map.Entry<String, Integer> entry: sorted.entrySet()) {
+            transactionsList.add(new Transaction(entry.getKey(), entry.getValue()));
+        }
+
+        transactionsList.sort((Transaction x, Transaction y) -> {return y.amount - x.amount;});
+
+        transactionsList.forEach(
+                (Transaction x) -> System.out.println(x.transaction + "_" + x.amount)
+        );
+
     }
 }
